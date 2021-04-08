5010e166b48a2d15691077d1c08ddf7dd6a157f5
denalibalser <denali.balser167@gmail.com>
2021-04-08 12:57:30 -0600

    change searchbar styling to tailwindcss

--- endangered-species-tracker-ui/src/presentationalComponents/SearchBar.js
+++ endangered-species-tracker-ui/src/presentationalComponents/SearchBar.js
@@ -2,20 +2,25 @@ import React from 'react'
 import SpeciesSearchResults from './SpeciesSearchResults'
 
 const SearchBar = (props) => {
-    const BarStyling = {margin: "0.75rem", width:"20rem", background:"#F2F1F9", border:"none", padding:"0.5rem"};
         
     return (
-        <>
-            <input 
-                style={BarStyling}
-                key="random1"
-                value={props.searchState.query}
-                placeholder={"search endangered species"}
-                onChange={(event) => props.changeState(event)}
-            />
+        <div>
+            <div className="p-8">
+                <div class="bg-white flex items-center rounded-full shadow-xl">
+                    <input 
+                        id="search"
+                        className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
+                        type="text"
+                        value={props.searchState.query}
+                        placeholder={"search endangered species"}
+                        onChange={(event) => props.changeState(event)}
+                    />  
+                </div>
+            </div>
             <SpeciesSearchResults results={props.searchState.filteredData} />
-        </>
+        </div>  
     ) 
 }
 
 export default SearchBar; 
+
