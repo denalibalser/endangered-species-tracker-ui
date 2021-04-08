14d91ed0c38668b2d6fe7cb4f03a959c7d27489d
denalibalser <denali.balser167@gmail.com>
2021-04-08 12:44:35 -0600

    fix spacing in speciessearchresults

--- endangered-species-tracker-ui/src/presentationalComponents/SpeciesSearchResults.js
+++ endangered-species-tracker-ui/src/presentationalComponents/SpeciesSearchResults.js
@@ -7,14 +7,15 @@ const SpeciesSearchResults = ({ results=[] }) => {
     
         return (
             <div className="h-48 flex flex-wrap content-start ...">
-                { results.map((data) => {
-                    if (data) {
-                        return (
-                            <Species key={data[5]} species={data}/>
-    	                )	
-    	            }
-                    return null
-                }) 
+                { results.map((data) => 
+                    {
+                        if (data) {
+                            return (
+                                <Species key={data[5]} species={data}/>
+    	                    )	
+    	                }
+                        return null
+                    }) 
                 }
             </div>
         )
