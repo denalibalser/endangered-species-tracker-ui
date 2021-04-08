5a25b55f9e464fe5fb0980bf181fa06588801feb
denalibalser <denali.balser167@gmail.com>
2021-04-08 10:26:16 -0600

    add loggedIn prop to Home component

--- endangered-species-tracker-ui/src/components/Navbar.js
+++ endangered-species-tracker-ui/src/components/Navbar.js
@@ -10,7 +10,11 @@ class Navbar extends Component {
             <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
                 <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                     <div className="text-sm lg:flex-grow">
-                            <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Home</Link>
+                            <Link to="/" 
+                                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" 
+                                loggedIn={this.props.loggedIn}>
+                                Home
+                            </Link>
                         {this.props.loggedIn ? (
                             <>
                                 <Link to="/dashboard" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Dashboard</Link>
