b7b6c38678a2c6ccd9fe9b999665f899a81e8814
denalibalser <denali.balser167@gmail.com>
2021-04-08 09:49:36 -0600

    remove unnecessary a tags from navbar component

--- endangered-species-tracker-ui/src/components/Navbar.js
+++ endangered-species-tracker-ui/src/components/Navbar.js
@@ -8,30 +8,19 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-                <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                    <div class="text-sm lg:flex-grow">
-                        <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                            <Link to="/">Home</Link>
-                        </a>
+                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div className="text-sm lg:flex-grow">
+                            <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Home</Link>
                         {this.props.loggedIn ? (
                             <>
-                                <a href="/dashboard" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                                    <Link to="/dashboard">Dashboard</Link>
-                                </a>
-                                <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                                    <Link to="#" onClick={() => this.props.logout(this.props.history)}>
-                                        Logout
-                                    </Link>
-                                </a>
+                                <Link to="/dashboard" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Dashboard</Link>
+                                <Link to="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                                onClick={() => this.props.logout(this.props.history)}>Logout</Link>
                             </>
                         ) : (
                             <>
-                                <a href="/signup" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                                    <Link to="/signup">Signup</Link>
-                                </a>
-                                <a href="/login" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                                    <Link to="/login">Login</Link>
-                                </a>
+                                <Link to="/signup" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Signup</Link>
+                                <Link to="/login" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Login</Link>
                             </>
                         )}
                     </div>
