7ad3653f661fdc43e579ab6436fbd4746a3d8b13
denalibalser <denali.balser167@gmail.com>
2021-04-16 14:58:50 -0600

    add alphabetical sort by species common name to render of species cards

--- endangered-species-tracker-ui/src/containerComponents/Dashboard.js
+++ endangered-species-tracker-ui/src/containerComponents/Dashboard.js
@@ -18,7 +18,7 @@ class Dashboard extends Component {
                 <h1 className="m-3"><strong>Welcome {this.props.user.first_name}</strong></h1>
                 <h2 className="m-3">Here are your saved species...</h2>        
                 <div className="h-48 flex flex-wrap content-start ...">
-                    {this.props.speciesCards.map(speciesCard => {
+                    {this.props.speciesCards.sort((a, b) => a.common_name.localeCompare(b.common_name)).map(speciesCard => {
                         if(speciesCard.user_id === this.props.user.id) {
                             return <SpeciesCard key={speciesCard.id} animal={speciesCard} remove={this.props.removeSpecies}/>
                         }
